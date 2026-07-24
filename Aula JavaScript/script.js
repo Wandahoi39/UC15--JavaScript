c254bab6f15e4e7447f0c6294a840787d562e1bc
Wandahoi (Vini) <rmvinicius1@gmail.com>
2026-07-24 13:10:58 -0300

    Add files via upload

---
+++ Aula JavaScript/script.js
@@ -0,0 +1,25 @@
+console.log("Hello, World!")
+console.log("Hi")
+alert("Erro no sistema") //alguem me desconfigurou
+confirm("Você deseja continuar?")
+
+let apelido = "Vinícius"
+console.log(apelido)
+
+let numero = 30+9
+console.log(numero)
+
+let idade = 16
+console.log(idade + "anos")
+
+console.log(10 < 5)
+
+let nome = prompt("Qual o seu nome?")
+console.log("Olá," + nome)
+
+if (nome == "Vinícius") {
+    alert("Aprovado")
+} else{
+    alert("Recusado")
+}
+
