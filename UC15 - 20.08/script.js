c24454d313d2c20787609fe0b1205ca7eb2474ca
Wandahoi (Vini) <rmvinicius1@gmail.com>
2026-08-20 10:15:44 -0300

    Add files via upload

---
+++ UC15 - 20.08/script.js
@@ -0,0 +1,21 @@
+// Com if/else - 4 linhas
+let mensagem;
+if (nota >= 6){
+    mensagem = 'Aprovado'
+} else {
+    mensagem = 'Reprovado'
+}
+
+// Com ternário - 1 linha
+const mensagem = nota >= 6 ? 'Aprovado' : 'Reprovado';
+
+//Atividade
+
+//Tarefa 1
+const descricao = temperatura > 30 ? 'Quente' : 'Agradável';
+
+//Tarefa 2
+const tipo = numero % 2 === 0 ? 'par' : 'impar';
+
+//Tarefa 3
+const saudacao = hora < 12 ? 'Bom dia' : 'Boa tarde/noite';
