d9ca3a7928d1a0b20ef921e4de6694fdfb59533a
Wandahoi (Vini) <rmvinicius1@gmail.com>
2026-09-11 12:27:13 -0300

    Add functions for media and travel cost calculations

--- UC15 - Funções/script.js
+++ UC15 - Funções/script.js
@@ -30,3 +30,50 @@ somar(5, 3)
 
 let resultafo = somar(5, 3);
 console.log(resultado);
+
+// Criar uma função que pegue dois valores e de a média se ele for acima de 6 é aprovado senão é reprovado
+
+function verificarMedia(nota1, nota2) {
+    let media = (nota1 + nota2) / 2;
+
+    if (media > 6) {
+        return "Aprovado";
+    } else {
+        return "Reprovado";
+    }
+}
+
+console.log(verificarMedia(8, 7));
+
+let nome = prompt("digite o nome do aluno.");
+let nota1 = Number("digite a primeira nota.");
+let nota2 = Number("digite a segunda nota.");
+
+let media1 = calcularMedia(nota1, nota2);
+
+console.log('$(nome) ficou com a media $(media1)');
+
+if (media1 >= 6) {
+    console.log('$(nome) está aprovado!');
+    
+} else {
+    console.log('$(nome) está reprovado!');
+}
+
+calcularMedia(nota1, nota2)
+
+// Crie um programa que ultilize uma função com vários parâmetros para calcular o custo de uma viagem. O programa deverá receber os valores de passagem, alimentação e passeios, calcular o total e informar se a viagem está dentro do orçamento de R$ 2.000,00
+
+function calcularViagem(passagem, alimentacao, passeios) {
+    let total = passagem + alimentacao + passeios;
+
+    console.log("Custo total da viagem: R$ " + total);
+
+    if (total <= 2000) {
+        console.log("A viagem está dentro do orçamento!");
+    } else {
+        console.log("A viagem ultrapassou o orçamento!");
+    }
+}
+
+calcularViagem(800, 600, 400);
