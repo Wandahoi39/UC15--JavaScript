1c80aa399f93a423046a0fd24abd1cb5aa01d38b
Wandahoi (Vini) <rmvinicius1@gmail.com>
2026-09-11 12:26:13 -0300

    Add files via upload

---
+++ UC15 - Funções/script.js
@@ -0,0 +1,32 @@
+console.log("Olá, Larissa! Seja bem vinda!")
+console.log("Olá, Levi! Seja bem vindo!")
+console.log("Olá, Julia! Seja bem vinda!")
+
+function darBoasVindas(nome) {
+    console.log(`Olá, ${nome}! Seja bem-vindo!`)
+}
+
+darBoasVindas("Levi");
+darBoasVindas("Julia");
+darBoasVindas("Larissa");
+
+
+function apresentar(nome, idade){
+    console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
+}
+
+function estudo(nome){
+    console.log(`${nome} está estudando.`)
+}
+
+estudo("Gaby")
+
+
+function somar(a, b) {
+    return a + b;
+}
+
+somar(5, 3)
+
+let resultafo = somar(5, 3);
+console.log(resultado);
