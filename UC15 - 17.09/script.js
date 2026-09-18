e6856cdc3423e06cf2b0f0e523a9946cc8e0f5c6
Wandahoi (Vini) <rmvinicius1@gmail.com>
2026-09-18 12:47:50 -0300

    Add files via upload

--- UC15 - 17.09/script.js
+++ UC15 - 17.09/script.js
@@ -7,3 +7,24 @@ function mudarcor() {
     document.getElementById("mensagem").style.color =
     "blue";
 }
+
+//--ontem, hj-->
+
+function mostrarNome() {
+    let nome = document.getElementById("nome").value;
+
+    document.getElementById("mensagem").textContent = "Olá, " + nome + "!";
+}
+
+
+let contador = 0;
+
+function aumentar() {
+    contador++;
+    document.getElementById("numero").textContent = contador;
+}
+
+function diminuir() {
+    contador--;
+    document.getElementById("numero").textContent = contador;
+}
